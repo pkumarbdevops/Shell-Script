e9e8983ad665531bb41f035ed99e3f155a958ec0
Praveen Kumar Devops <Praveen Kumar Devops>
2023-09-01 18:15:52 +0530

    Hello World Script Addeded

--- hello-world.sh
+++ hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello World,I am Learning DevOps With AWS"
+
+echo "Ramesh : Hi Suresh, Good Morning"
+echo "Suresh : Good Morning Ramesh "
+echo "Ramesh : How are you Suresh "
+echo "Suresh : I am good, How are you Ramesh "
