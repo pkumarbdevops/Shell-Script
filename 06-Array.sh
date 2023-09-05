c88ed979d4ca61a300de332f123c898abd94732d
Praveen Kumar Devops <Praveen Kumar Devops>
2023-09-05 19:02:22 +0530

    Hello World Script Addd

--- 06-Array.sh
+++ 06-Array.sh
@@ -6,5 +6,8 @@ PERSON=("Ramesh" "Suresh" "Sachin")
 
 # inside arran we always refer value with index and it satrt from 0
 
-echo "First Person : ${PERSON[0]}"
+# echo "First Person : ${PERSON[0]}" 
 
+# @ means all the persons in the array 
+
+echo "First Person : ${PERSON[@]}" 
