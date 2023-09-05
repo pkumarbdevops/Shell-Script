f608a1400fe925024f9dfa74156af987c525413b
Praveen Kumar Devops <Praveen Kumar Devops>
2023-09-05 17:28:51 +0530

    Hello World Script Addd

--- 02-Variables.sh
+++ 02-Variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#Shell script run the commnad inside $() and stored the output into variable called date
+#Shell script run the commnad inside $() and stored the output into variable called DATE
 DATE=$(date)
 echo "Hello, this script is executed at timestamp: $DATE"
