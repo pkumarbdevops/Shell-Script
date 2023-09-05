61cf4b14c30490507e97670fbde6f9785d7ec328
Praveen Kumar Devops <Praveen Kumar Devops>
2023-09-05 18:59:19 +0530

    Hello World Script Addd

--- 05-Username.sh
+++ 05-Username.sh
@@ -4,6 +4,7 @@ echo "Please eneter your username"
 
 read -s USERNAME # THIS IS THE VARIABLE WHERE THE VALUE OF THE ONE USER ENTERED
 
+#this is only for validation, we should not print in the terminal
 echo "Username entered is: $USERNAME"
 
 echo "Please enter your password"
---
+++ 06-Array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#this is is called array, instead of single value it can hold multiple values 
+
+PERSON=("Ramesh" "Suresh" "Sachin") 
+
+# inside arran we always refer value with index and it satrt from 0
+
+echo "First Person : ${PERSON[0]}"
+
