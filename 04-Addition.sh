ca4162684947ded83c79a2fbb3de40d08308ca7e
Praveen Kumar Devops <Praveen Kumar Devops>
2023-09-05 18:02:00 +0530

    Hello World Script Addd

---
+++ 04-Addition.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# this is the comment, we need to develop a programm that can do addition of 2 number
+
+NUMBER=$1 # passing the values from the command line
+NUMBER=$2 # passing the values from the command line
+# I need to runa command inside shell to add these 2 number
+SUM=$((NUMBER1+NUMBER2))
